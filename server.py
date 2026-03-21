b7aa403153f18b0e97969a230f38fe70c2a14964
odanree <nightseek@yahoo.com>
2026-03-20 20:35:43 -0700

    chore: trigger deploy

--- server.py
+++ server.py
@@ -365,3 +365,4 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 8888))
     print(f"🚀 Starting ADU Dashboard Server (FastAPI) on port {port}")
     uvicorn.run('server:app', host='0.0.0.0', port=port, reload=False)
+
